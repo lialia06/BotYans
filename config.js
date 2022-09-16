aecea9ccdb8da4fed11f1d9ebdfacc12b0b38b3d
lialia06 <rachrisgamer@gmail.com>
2022-09-16 18:36:01 +0700

    BotYans

---
+++ config.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Other
+global.owner = ['6288239560059']
+global.premium = ['6288239560059']
+global.ownername = 'Yans'
+global.botname = 'LiaBot'
+global.packname = '© by LiaBot'
+global.gc = '-'
+global.linkyt = 'https://youtube.com/channel/'
+global.linkgc = 'https://chat.whatsapp.com/'
+global.limitawal = '100'
+global.author = '@yans'
+global.sessionName = 'polo'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = 'terserah'
+global.mess = {
+    success: 'Berhasil',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu sebentar, sedang di proses',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.gambar = 'https://telegra.ph//file/e50a028587f4f745c269e.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
+global.sc = fs.readFileSync('./media/sc.jpg')
+global.tq = fs.readFileSync('./media/tq.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.thumb = { url: 'https://j.top4top.io/p_2449va1b66.jpg' }
+global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
